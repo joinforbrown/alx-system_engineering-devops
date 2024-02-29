c39ca37ce6bcb2350e8f3d4fe657952472a6e951
joinforbrown <callme508charles@gmail.com>
2024-02-29 07:06:53 -0800

    Task 1 updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+def match_school(str)
+  regex = /School/
+  str.scan(regex) { |match| puts match }
+end
+
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+match_school(ARGV[0])
+
