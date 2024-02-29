7cdfb17136f1ff34a4496f3e856e6622389fcfbc
joinforbrown <callme508charles@gmail.com>
2024-02-29 07:13:02 -0800

    Task 1 updated

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,5}n/).join
+
+# Define the regular expression pattern
+pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+# Take the argument from the command line
+input = ARGV[0]
+
+# Match the argument against the regular expression
+if input =~ pattern
+  puts "The input matches the pattern."
+else
+  puts "The input does not match the pattern."
+end
+
